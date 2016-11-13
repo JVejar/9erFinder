91282689d8365b379b1aa2a487d674654d797d94
jvejar <jvejar@uncc.edu>
2016-11-13 20:06:26 +0000

    Commit Nov 13 Sunday

---
+++ app.js
@@ -0,0 +1,14 @@
+(funtion(){
+    
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDAFx-7Q6V_BKIbs43PSkfTzK6oMuHIhWY",
+    authDomain: "ninerfinder-e34bc.firebaseapp.com",
+    databaseURL: "https://ninerfinder-e34bc.firebaseio.com",
+    storageBucket: "ninerfinder-e34bc.appspot.com",
+    messagingSenderId: "408466489926"
+  };
+  firebase.initializeApp(config);
+  
+  
+}());
